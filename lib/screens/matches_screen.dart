5c196a77ef68708901226c70e957223097e25511
André Camargo <andreribeirocamargo@gmail.com>
2021-08-28 20:47:19 -0300

    Remove dead code

--- lib/screens/matches_screen.dart
+++ lib/screens/matches_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:intl/intl.dart';
 import 'package:wakelock/wakelock.dart';
 
 import '../matches_storage.dart';
