9062d901bf4ba368bfecbbddf655bc57f53de317
André Camargo <andreribeirocamargo@gmail.com>
2022-11-23 07:24:33 -0300

    Add setup.sh

--- setup.sh
+++ setup.sh
@@ -16,6 +16,16 @@ read -n 1 -r -s -p $'Press RETURN to continue or CONTROL+C to abort...\n'
 
 brew install --cask flutter
 
+brew tap homebrew/cask-versions
+brew install --cask temurin11
+brew install --cask android-commandlinetools
+brew install --cask android-platform-tools
+sdkmanager "build-tools;30.0.3" "platform-tools" "emulator" "system-images;android-30;google_apis;x86_64" "platforms;android-30"
+
+# https://gist.github.com/mrk-han/66ac1a724456cadf1c93f4218c6060ae
+sdkmanager --install "system-images;android-25;google_apis;x86"
+echo "no" | avdmanager --verbose create avd --force --name "generic_4.4" --package "system-images;android-25;google_apis;x86" --tag "google_apis" --abi "x86"
+
 mkdir -p ~/dev/flutter
 cd ~/dev/flutter
 git clone https://github.com/acamargo/adriana.git
